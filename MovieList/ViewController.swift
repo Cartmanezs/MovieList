1e62b7a73f998ff01ccaa8206309f08faa37d805
Ihor Palyvoda <ihor.palyvoda@perfsol.tech>
2023-10-11 12:28:12 +0300

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Igor Palyvoda on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
