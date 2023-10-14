65c644c302a462c70c7ce719a989d7f85c10dfc8
Ihor Palyvoda <ihor.palyvoda@perfsol.tech>
2023-10-14 19:30:02 +0300

    Added player UI

---
+++ MovieList/MainModule/View/PlayerView.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerView.swift
+//  MovieList
+//
+//  Created by Igor Palyvoda on 13.10.2023.
+//
+
+import Foundation
---
+++ MovieList/MainModule/ViewControllers/PlayerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerViewController.swift
+//  MovieList
+//
+//  Created by Igor Palyvoda on 13.10.2023.
+//
+
+import Foundation
