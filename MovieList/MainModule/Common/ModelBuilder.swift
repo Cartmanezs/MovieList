5581b96ab29988b6d7ba21bfa059d20e15696487
Ihor Palyvoda <ihor.palyvoda@perfsol.tech>
2023-10-16 12:40:45 +0300

    configure on launch logic

--- MovieList/MainModule/Common/ModelBuilder.swift
+++ MovieList/MainModule/Common/ModelBuilder.swift
@@ -5,4 +5,29 @@
 //  Created by Igor Palyvoda on 16.10.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol Builder {
+    static func createMainModule() -> UIViewController
+    static func createDetailModule() -> UIViewController
+}
+
+class ModelBuilder: Builder {
+    static func createMainModule() -> UIViewController {
+        if let mainMovieViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainMovieViewController") as? MainMovieViewController {
+            let apiService = NetworkService()
+            let movieViewModel = MovieViewModel(apiService: apiService)
+            mainMovieViewController.movieViewModel = movieViewModel
+            return mainMovieViewController
+        }
+        return UIViewController()
+    }
+    
+    static func createDetailModule() -> UIViewController {
+        if let detailMovieViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailMovieViewController") as? DetailMovieViewController {
+            return detailMovieViewController
+        }
+
+        return UIViewController()
+    }
+}
